3811ead1107d977979ef5fcd1a014e689e922038
Dennis Jakobi <dennis.jakobi76@gmail.com>
2024-12-17 15:38:14 +0100

    added back-arrow to impressum.html and privacy-policy.html and click-function to get back to last visited site

--- scripts/db_tasks.js
+++ scripts/db_tasks.js
@@ -1,5 +1,5 @@
-let tasks = {
-    /*Generated Id*/ : {
+let tasks = [
+    /*Generated Id :*/ {
         "type": "userStory", // || "technicalTask"
         "title": "Kochwelt Page & Recipe Recommender",
         "description": "Build start page recipe recommendation",
@@ -9,35 +9,38 @@ let tasks = {
         "subtasks": [
             {
                 "description": "Implement Recipe Recommendation",
-                "checked": true
+                "checked": true,
             },
             {
                 "description": "Start Page Layout",
-                "checked": false
-            }
+                "checked": false,
+            },
         ],
         "state": "backlog", // || "todo" || "testing" || "done"
     },
     {
-        id: 1,
-        type: "technicalTask",
-        title: "CSS Archiceture Planning",
-        description: "Define CSS naming conventions and structure",
-        dueDate: "02.09.2023",
-        priority: "urgent",
-        assignedTo: ["Sofia Müller (You)", "Benedikt Ziegler"],
-        subtasks: ["Establish CSS Methodology", "Setup Base Styles"],
-        state: "todo",
+        "type": "technicalTask",
+        "title": "CSS Archiceture Planning",
+        "description": "Define CSS naming conventions and structure",
+        "dueDate": "02.09.2023",
+        "priority": "urgent",
+        "assignedTo": ["Sofia Müller (You)", "Benedikt Ziegler"],
+        "subtasks": [
+            {
+                "description": "Establish CSS Methodology",
+            },
+            { "description": "Setup Base Styles" },
+        ],
+        "state": "todo",
     },
     {
-        id: 2,
-        type: "technicalTask",
-        title: "Implement Recipe Recommendation",
-        description: "Define Recipe Recommendation structure and generate render-function",
-        dueDate: "12.09.2023",
-        priority: "medium",
-        assignedTo: ["Anja Schulz", "David Eisenberg"],
-        subtasks: [],
-        state: "todo",
+        "type": "technicalTask",
+        "title": "Implement Recipe Recommendation",
+        "description": "Define Recipe Recommendation structure and generate render-function",
+        "dueDate": "12.09.2023",
+        "priority": "medium",
+        "assignedTo": ["Anja Schulz", "David Eisenberg"],
+        "subtasks": [],
+        "state": "todo",
     },
-};
+];
