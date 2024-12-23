b636d0d12a8481586b01c56d448b2ee2e0f12fc8
Ping <dev.bastianabel@gmail.com>
2024-12-23 22:38:42 +0100

    toggleclass function added in standart.js

---
+++ scripts/standart.js
@@ -0,0 +1,6 @@
+function toggleClassById(className, ...IDs) {
+    for (let i = 0; i < IDs.length; i++) {
+        currentElement = document.getElementById(IDs[i]);
+        currentElement?.classList.toggle(className);
+    }
+}
