42900652a0113b4fd7aff8a6b345ef885c318e7b
Ping <dev.bastianabel@gmail.com>
2025-01-02 02:50:14 +0100

    finished password visibility logic and added it to the login and sign up html

--- scripts/register.js
+++ scripts/register.js
@@ -54,3 +54,28 @@ async function checkIfEmailExists(email) {
 function navigateToLogin() {
     window.location.href = "login-page.html";
 }
+
+function checkforvisibility(element, imgId) {
+    inputRefValue = element.value;
+    lockImgRef = document.getElementById("password-lock-" + imgId);
+    visibilityImgRef = document.getElementById("password-visibility-" + imgId);
+    if(inputRefValue.length === 0) {
+        lockImgRef.classList.remove("d-none");
+        visibilityImgRef.classList.add("d-none");
+    }else{
+        lockImgRef.classList.add("d-none");
+        visibilityImgRef.classList.remove("d-none");
+    }
+}
+
+function changePasswordVisibility(inputfieldId, imgElement) {
+    visibleImgRef = imgElement;
+    inputfieldRef = document.getElementById(inputfieldId);
+    if(inputfieldRef.type == "text") {
+        inputfieldRef.type = "password";
+        visibleImgRef.src = "/assets/icons/visibility_off.svg"
+    }else{
+        inputfieldRef.type = "text";
+        visibleImgRef.src = "/assets/icons/visibility_on.svg"
+    }
+}
