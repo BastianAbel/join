c2ea00cdf35eac5c0933a8a80a57467ad41c529e
Ping <dev.bastianabel@gmail.com>
2025-01-03 05:58:45 +0100

    fixed minor bugs in register.js and rewritten it for more readability

--- scripts/register.js
+++ scripts/register.js
@@ -1,84 +1,126 @@
-function checkIfPasswordsMatch() {
-    let pw = document.getElementById("password").value;
-    let confirmPw = document.getElementById("confirmPassword").value;
-    document.getElementById('password').style.border = "1px solid #ccc";
-    document.getElementById('confirmPassword').style.border = "1px solid #ccc";
-    if (pw === "" || confirmPw === "") {
-        document.getElementById("pw-state-message").classList.add('d-none');
-    } else if (pw === confirmPw) {
-        passwordClassesGreen();
-    }
-    else {
-        passwordClassesRed();
-    }
-}
-// klassen passwort stimmt
-function passwordClassesGreen() {
-    document.getElementById('pw-state-message').innerHTML = "Passwords match!"
-    document.getElementById("pw-state-message").classList.remove('d-none');
-    document.getElementById('pw-state-message').classList.add('pw-match-green');
-    document.getElementById('password').style.border = "1px solid var(--icon-low-green)";
-    document.getElementById('confirmPassword').style.border = "1px solid var(--icon-low-green)";
-}
-// klassen passwort stimmt nicht
-function passwordClassesRed() {
-    document.getElementById('pw-state-message').innerHTML = "Your passwords don't match. Please try again."
-    document.getElementById('password').style.border = "1px solid var(--icon-urgent-red)";
-    document.getElementById('confirmPassword').style.border = "1px solid var(--icon-urgent-red)";
-    document.getElementById("pw-state-message").classList.remove('d-none');
-    document.getElementById('pw-state-message').classList.remove('pw-match-green');
-}
-// User im Backend anlegen
-function addUser() {
-    let userName = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    checkIfEmailExists(userName, email, password);
-}
-//check if email is already existing
-async function checkIfEmailExists(userName, email, password) {
-    await fetchUsers();
-    let userEmail = users.find(user => user.user.email === email);
-    console.log(userEmail)
-    if (userEmail) {
-        document.getElementById('pw-state-message').innerHTML = "This email already exists."
-        document.getElementById('pw-state-message').classList.remove('pw-match-green');
-        return false;
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const userNameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordFeedbackRef = document.getElementById("pw-state-message");
+const signupPopup = document.getElementById("register-popup");
+
+function visualizeIfPasswordsMatch() {
+    const passwordInputValue = getPasswordInput();
+    const isEmpty = passwordFieldsEmpty(passwordInputValue);
+    if (isEmpty) {
+        removePasswordFeedbackStyle()
     } else {
-        document.getElementById('pw-state-message').innerHTML = ""
-        let userData = { "name": `"${userName}"`, "email": `"${email}"`, "password": `"${password}"` };
-        postData(path = "users/", data = { userData });
-        document.getElementById("register-popup").classList.remove('d-none');
-        setTimeout(navigateToLogin, 800)
-        return true;
+        (PasswordsMatch(passwordInputValue) ? setPasswordFeedbackStyle("matching") : setPasswordFeedbackStyle("notMatching"))
     }
 }
 
-// Weiterleitung
+function getPasswordInput() {
+    password = passwordInput.value;
+    confirmPassword = confirmPasswordInput.value;
+    return { "password": password, "confirmPassword": confirmPassword }
+}
+
+function passwordFieldsEmpty(inputToTest) {
+    return checkIfpasswordFieldsEmpty(inputToTest)
+}
+
+function checkIfpasswordFieldsEmpty(inputToTest) {
+    return inputToTest.password === "" || inputToTest.confirmPassword === ""
+}
+
+function removePasswordFeedbackStyle() {
+    passwordInput.style.border = "";
+    confirmPasswordInput.style.border = "";
+    removeFeedbackText();
+}
+
+function removeFeedbackText() {
+    passwordFeedbackRef.classList.add('d-none');
+}
+
+function PasswordsMatch(inputToTest) {
+    return checkIfPasswordsMatch(inputToTest)
+}
+
+function checkIfPasswordsMatch(inputToTest) {
+    return inputToTest.password === inputToTest.confirmPassword
+}
+
+const PasswordFeedbackStyles = {
+    matching: {
+        text: "Passwords match!",
+        colorcode: "--icon-low-green"
+    },
+    notMatching: {
+        text: "Your passwords don't match. Please try again.",
+        colorcode: "--icon-urgent-red"
+    }
+}
+
+function setPasswordFeedbackStyle(currentState) {
+    passwordFeedbackRef.innerHTML = PasswordFeedbackStyles[currentState].text;
+    passwordFeedbackRef.classList.remove('d-none');
+    passwordInput.style.border = "1px solid var(" + PasswordFeedbackStyles[currentState].colorcode + ")";
+    confirmPasswordInput.style.border = "1px solid var(" + PasswordFeedbackStyles[currentState].colorcode + ")";
+    passwordFeedbackRef.classList.toggle('pw-match-green', currentState === "matching");
+}
+
+async function processSignUp() {
+    if (await emailExists()) {
+        setEmailExistsFeedback();
+    } else {
+        removeFeedbackText();
+        addNewProfileToServer();
+        showSignUpPopUp();
+        setTimeout(navigateToLogin, 800);
+    }
+}
+
+async function emailExists() {
+    return await checkIfEmailExists()
+}
+
+async function checkIfEmailExists() {
+    await fetchUsers();
+    emailToSearch = emailInput.value;
+    return !!users.find(user => user.user.email === emailToSearch);
+}
+
+function setEmailExistsFeedback() {
+    passwordFeedbackRef.innerHTML = "This email already exists."
+    passwordFeedbackRef.classList.remove('pw-match-green');
+    passwordFeedbackRef.classList.remove('d-none');
+}
+
+function showSignUpPopUp() {
+    signupPopup.classList.remove('d-none');
+}
+
+function addNewProfileToServer() {
+    let userData = { "name": `"${userNameInput.value}"`, "email": `"${emailInput.value}"`, "password": `"${passwordInput.value}"` };
+    postData(path = "users/", data = { userData });
+}
+
 function navigateToLogin() {
     window.location.href = "login-page.html";
 }
 
-function checkforvisibility(element, imgId) {
+function togglePasswordVisibilityIcon(element, imgId) {
     inputRefValue = element.value;
     lockImgRef = document.getElementById("password-lock-" + imgId);
     visibilityImgRef = document.getElementById("password-visibility-" + imgId);
-    if(inputRefValue.length === 0) {
-        lockImgRef.classList.remove("d-none");
-        visibilityImgRef.classList.add("d-none");
-    }else{
-        lockImgRef.classList.add("d-none");
-        visibilityImgRef.classList.remove("d-none");
-    }
+    lockImgRef.classList.toggle("d-none", !(inputRefValue.length === 0));
+    visibilityImgRef.classList.toggle("d-none", (inputRefValue.length === 0));
 }
 
 function changePasswordVisibility(inputfieldId, imgElement) {
     visibleImgRef = imgElement;
     inputfieldRef = document.getElementById(inputfieldId);
-    if(inputfieldRef.type == "text") {
+    if (inputfieldRef.type == "text") {
         inputfieldRef.type = "password";
         visibleImgRef.src = "/assets/icons/visibility_off.svg"
-    }else{
+    } else {
         inputfieldRef.type = "text";
         visibleImgRef.src = "/assets/icons/visibility_on.svg"
     }
