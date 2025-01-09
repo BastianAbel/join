de8db73d990fd7640c083cf7efb973547a51141c
Leon Dorsch <leon.dorsch@gmx.de>
2025-01-09 14:16:02 +0100

    edit contact view design

--- scripts/contact_list.js
+++ scripts/contact_list.js
@@ -48,7 +48,7 @@ function contactBigView(name, email, initials, id){
 function editBigView(initials, color){
     document.getElementById('edit-delete-menu').style.display = "none";
     document.getElementById('profileBtn').style.backgroundColor = "#b8b9bb";
-    document.getElementById('edit-contact-overlay').classList.remove('d-none')
+    document.getElementById('edit-contact-overlay').classList.remove('d-none');
     document.getElementById('main-content').innerHTML += showEditContactView(initials, color);
 }
 
@@ -59,14 +59,67 @@ function closeEditContactView(){
     document.getElementById('editContactContainer').classList.add('d-none');
 }
 
+function EditContactViewSlideDown(){
+    document.getElementById('editContactContainer').classList.add('hidden');
+    setTimeout(() => {
+        closeEditContactView();
+    }, 300);
+}
+
 window.addEventListener('mouseup', function (e) {
     let editContactDiv = document.getElementById('editContactContainer');
-    if (!editContactDiv.contains(e.target)) {
-        closeEditContactView();
+    if (editContactDiv && !editContactDiv.contains(e.target)) {
+        editContactDiv.classList.add('hidden');
+        setTimeout(() => {
+            closeEditContactView();
+        }, 300); 
     }
 });
 
-
 function navigateToContactList(){
     window.location.href = "contactlist.html"
+}
+
+let submenuVisible = false;
+
+function showSubmenu() {
+    let submenu = document.getElementById('submenu');
+    submenu.classList.remove('d-none', 'hidden');
+    submenuVisible = true; 
+}
+
+document.addEventListener('mouseup', function (e) {
+    let submenuDiv = document.getElementById('submenu');
+    if (submenuVisible && !submenuDiv.contains(e.target)) {
+        submenuDiv.classList.add('hidden');
+        setTimeout(() => {
+            submenuDiv.classList.add('d-none');
+            submenuVisible = false; 
+        }, 100); 
+    }
+});
+
+let editDeleteMenuVisible = false;
+
+function showEditDeleteMenu(){
+    let editDeleteMenu = document.getElementById('edit-delete-menu');
+    document.getElementById('option-circle').style.display = "none";
+    editDeleteMenu.classList.remove('d-none', 'hidden');
+    editDeleteMenuVisible = true;
+}
+
+window.addEventListener('mouseup', function (e) {
+    let editDeleteMenuDiv = document.getElementById('edit-delete-menu');
+    if (editDeleteMenuVisible && !editDeleteMenuDiv.contains(e.target)) {
+        editDeleteMenuDiv.classList.add('hidden');
+        setTimeout(() => {
+            editDeleteMenuDiv.classList.add('d-none');
+            document.getElementById('option-circle').style.display = "flex";
+            editDeleteMenuVisible = false; 
+        }, 100); 
+    }
+});
+
+function openAddContactView(){
+    document.getElementById('main-content').innerHTML += showEditContactView();
 }
--- scripts/standart.js
+++ scripts/standart.js
@@ -19,43 +19,5 @@ function onlyLoadIfUserOrGuest() {
     }
 }
 
-let submenuVisible = false;
 
-function showSubmenu() {
-    let submenu = document.getElementById('submenu');
-    submenu.classList.remove('d-none', 'hidden');
-    submenuVisible = true; 
-}
-
-document.addEventListener('mouseup', function (e) {
-    let submenuDiv = document.getElementById('submenu');
-    if (submenuVisible && !submenuDiv.contains(e.target)) {
-        submenuDiv.classList.add('hidden');
-        setTimeout(() => {
-            submenuDiv.classList.add('d-none');
-            submenuVisible = false; 
-        }, 100); 
-    }
-});
-
-let editDeleteMenuVisible = false;
-
-function showEditDeleteMenu(){
-    let editDeleteMenu = document.getElementById('edit-delete-menu');
-    document.getElementById('option-circle').style.display = "none";
-    editDeleteMenu.classList.remove('d-none', 'hidden');
-    editDeleteMenuVisible = true;
-}
-
-window.addEventListener('mouseup', function (e) {
-    let editDeleteMenuDiv = document.getElementById('edit-delete-menu');
-    if (editDeleteMenuVisible && !editDeleteMenuDiv.contains(e.target)) {
-        editDeleteMenuDiv.classList.add('hidden');
-        setTimeout(() => {
-            editDeleteMenuDiv.classList.add('d-none');
-            document.getElementById('option-circle').style.display = "flex";
-            editDeleteMenuVisible = false; 
-        }, 100); 
-    }
-});
 
