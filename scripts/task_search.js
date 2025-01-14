e36aa06fa494504fad6889bc3da473e99a950f98
SantaFee94 <timokosfeld11@gmail.com>
2025-01-14 11:34:04 +0100

    Optimization of progress bar calculation and adjustment of display of subtasks in board.js and task_search.js; CSS adjustments for subtask display

--- scripts/task_search.js
+++ scripts/task_search.js
@@ -106,12 +106,19 @@ function checkUserSearchInputAndRedirect() {
 }
 
 function getSubtaskStatus(subtasks) {
+    let completedSubtasks = 0;
+    let totalSubtasks = 0;
+    if (subtasks === undefined) {
+        statusProgressBar(completedSubtasks, totalSubtasks)
+        return `<span>Keine Subtasks</span>`;
+    }
+
     if (!subtasks || subtasks.length === 0) {
         return `<span>Keine Subtasks</span>`;
     }
 
-    const completedSubtasks = subtasks.filter((subtask) => subtask.checked).length;
-    const totalSubtasks = subtasks.length;
+    completedSubtasks = subtasks.filter((subtask) => subtask.checked).length;
+    totalSubtasks = subtasks.length;
     statusProgressBar(completedSubtasks, totalSubtasks);
     return `<span id="state">${completedSubtasks}/${totalSubtasks} Subtasks</span>`;
 }
