c78893f6052ebcddb84f3ee1860551892fa20022
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-01-15 11:24:42 +0100

    bugfix

--- scripts/board.js
+++ scripts/board.js
@@ -5,7 +5,7 @@ function taskBigView() {
 }
 
 function closeTaskBigView() {
-    document.getElementById("task-big-container").outerHTML = "";
+    document.getElementById("task-big-container").classList.add("d-none");
 }
 
 function navigateToBoard() {
@@ -30,7 +30,7 @@ function writeCardsToBoardSectionsFromArray(array) {
     for (let j = 0; j < array.length; j++) {
         let renderValuesObject = getObjectWithValuesNeedeInBoardCard(array[j]);
         if (array[j].state === "toDo") {
-            hideElementAndRenderAnother("todo", "board-to-do-section", renderValuesObject.task, renderValuesObject.subtaskState, renderValuesObject.prioImg, renderValuesObject.employeesName, progressBarCalc, array[j].type, j);
+            hideElementAndRenderAnother("todo", "board-to-do-section", renderValuesObject.task, renderValuesObject.subtaskState, renderValuesObject.prioImg, renderValuesObject.employeesName, progressBarCalc);
         } else if (array[j].state === "inProgress") {
             hideElementAndRenderAnother("inProgress", "board-in-progress-section", renderValuesObject.task, renderValuesObject.subtaskState, renderValuesObject.prioImg, renderValuesObject.employeesName, progressBarCalc);
         } else if (array[j].state === "awaitFeedback") {
@@ -52,5 +52,15 @@ function getObjectWithValuesNeedeInBoardCard(task) {
 
 function hideElementAndRenderAnother(elementToHide, parentToRenderCardsIn, renderParam_1, renderParam_2, renderParam_3, renderParam_4, renderParam_5) {
     document.getElementById(elementToHide).classList.add("d-none");
-    document.getElementById(parentToRenderCardsIn).innerHTML += taskCardTemplateToHtml(renderParam_1, renderParam_2, renderParam_3, renderParam_4, renderParam_5); 
+    document.getElementById(parentToRenderCardsIn).innerHTML += taskCardTemplateToHtml(renderParam_1, renderParam_2, renderParam_3, renderParam_4, renderParam_5);
+}
+
+function makeItDraggable() {
+    //TODO - alle Karten brauchen draggable=true;
+    //TODO - alle Karten brauchen onDragStart;
+    //TODO - alle Ziellbereich brauchen allowDrop und onDrop;
+    //TODO - Scroll-Mechanik steht in Dev-Test
+    //TODO - bei Drop muss der neue State in Firebase gespeichert werden. Dann müssen die neuen Werte in den Sessionstorage. Und danach muss das Board geupdatet werden
+    //TODO - Wenn alles klappt, erhalten die Cards den Drag-Effekt (leicht schräg)
+    //TODO - onDragEnd wird der Drag-Effekt wieder entnommen (gerade Karte)
 }
