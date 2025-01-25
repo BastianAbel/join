ec08a1d90364f01a22c6822a1d3c296935a2e52e
SantaFee94 <timokosfeld11@gmail.com>
2025-01-25 22:57:37 +0100

    Refactor task card template and user container creation; improve CSS styles for better layout and visibility

--- scripts/board_task_template.js
+++ scripts/board_task_template.js
@@ -2,7 +2,7 @@ function taskCardTemplateToHtml(task, state, priorityImage, employeesName, progr
     return ` 
     <div id="${task.id}" onclick="taskBigView('${task.id}','${j}', '${task.dueDate}', '${task.priority}', '${priorityImage}', '${encodeURIComponent(JSON.stringify(task.assignedTo))}', '${encodeURIComponent(JSON.stringify(task.subtasks))}', '${cardTypeColor}')" class="card-main-container draggable" draggable="true" ondragstart="startDragging(event, '${
         task.id
-    }')" ondrag="enableScrollByDragging(event)" onmousedown="rotate(event)" onmouseup="removeRotations()" onmouseover="enableScrollByMouseposition(event)">
+    }')" ondrag="enableScrollByDragging(event)" onmousedown="rotate(event)" onmouseup="removeRotations()" >
             <div class="card-main-container-content">
                 <div style=" ${cardTypeColor}" class="labels-board-card-label">
                     <div id="task-type${j}" class="card-label"><span>${capitalizeFirstLetter(task.type)}</span></div>
