d64fd0c8e63ae6b10c5f47fe61ee3097edf0d0a2
Bastian Abel <dev.bastianabel@gmail.com>
2025-02-02 13:28:50 +0100

    add contact removed html5 validation

--- scripts/contact_list_template.js
+++ scripts/contact_list_template.js
@@ -196,7 +196,7 @@ function renderAddContactView() {
                     </div>
                     <div class="input-feedback-container" id="input-feedback-container"></div>
                     <div class="button-container">
-                        <button type="submit" class="create-contact-button" disabled>
+                        <button type="submit" class="create-contact-button">
                             Create contact
                             <img src="/assets/icons/check.svg" alt="">
                         </button>
